8cdc707294d3c4aee85b26f588ed760e6e17254e
big <chanyut@x10studio.com>
2015-01-17 23:49:32 +0700

    Hello world

---
+++ Assets/HelloWorld/Scripts/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Awake() {
+        Debug.Log("Hello world - Awake");
+    }
+
+    void Start()
+    {
+        Debug.Log("Hello world - Start");
+    }
+
+    void Update()
+    {
+        Debug.Log("Hello world - Update");
+    }
+}
