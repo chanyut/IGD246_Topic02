bab3ce94db9c568c19b25777a1967652e2396231
big <chanyut@x10studio.com>
2015-02-14 13:33:54 +0700

    add 2 new projects: PickTheBall and BrickGame

---
+++ Assets/PickTheBallGame/Scripts/BallSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace PickTheBallGame
+{
+    public class BallSpawner : MonoBehaviour
+    {
+        public Transform[] SpawnPoints;
+        public GameObject BallPrefab;
+
+        private float mTimer;
+
+        void Start()
+        {
+            mTimer = 0;
+        }
+
+        void Update()
+        {
+            mTimer += Time.deltaTime;
+            if (mTimer >= 1f)
+            {
+                int l = SpawnPoints.Length;
+                int index = Random.Range(0, l);
+                Instantiate(BallPrefab, SpawnPoints[index].position, Quaternion.identity);
+                mTimer = 0;
+            }
+        }
+    }
+}
---
+++ Assets/PickTheBallGame/Scripts/BasketController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace PickTheBallGame
+{
+    public class BasketController : MonoBehaviour
+    {
+        public float Speed;
+
+        // Update is called once per frame
+        void Update()
+        {
+            float h = Input.GetAxis("Horizontal");
+            Vector3 direction = new Vector3(h, 0, 0);
+            transform.Translate(h * Speed * Time.deltaTime, 0, 0);
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Ball")
+            {
+                Debug.Log("Ball enter");
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
